081fc3919599421147939c678e2be6820c796859
mavillan <mavillan@alumnos.inf.utfsm.cl>
2016-12-24 03:14:32 -0300

    added numba evaluation functions for u and his derivatives. Also added load_function.
    Former-commit-id: 4204f1b4e3dc0c8283ccd1cec66fe12aacd6db4a

--- lib/utils.py
+++ lib/utils.py
@@ -13,15 +13,44 @@ supp = 5.      # gaussians support
 minsig = 0.001 # guassians minimal broadening
 
 @numba.jit('float64[:] (float64[:], float64[:], float64[:], float64[:], float64[:], float64[:], float64, float64)', nopython=True)
-def evaluate(c, sig, xe, ye, xc, yc, supp=5., sig0=0.001):
+def u_eval(c, sig, xe, ye, xc, yc, supp=supp, sig0=minsig):
     m = len(xe)
     n = len(xc)
     ret = np.zeros(m)
     for i in range(m):
         for j in range(n):
-            if (xe[i]-xc[j])**2 + (ye[i]-yc[j])**2 > supp**2 * sig[j]**2: continue
-            ret[i] += c[j] * exp( -( (xe[i]-xc[j])**2 + (ye[i]-yc[j])**2 ) / (2* (sig0**2 + sig[j]**2) ) )
+            dist2 = (xe[i]-xc[j])**2 + (ye[i]-yc[j])**2
+            if  dist2 > supp**2 * sig[j]**2: continue
+            ret[i] += c[j] * exp( -dist2 / (2* (sig0**2 + sig[j]**2) ) )
     return ret 
+
+
+@numba.jit('float64[:,:] (float64[:], float64[:], float64[:], float64[:], float64[:], float64[:], float64, float64)', nopython=True)
+def u_eval_full(c, sig, xe, ye, xc, yc, supp=supp, sig0=minsig):
+    m = len(xe)
+    n = len(xc)
+    ret = np.zeros((6,m))
+    for i in range(m):
+        for j in range(n):
+            dist2 = (xe[i]-xc[j])**2 + (ye[i]-yc[j])**2
+            if  dist2 > supp**2 * sig[j]**2: continue
+            # common terms
+            aux1 = (-1./(sig0**2+sig[j]**2))
+            aux2 = aux1**2
+            # u evaluation
+            ret[0,i] += c[j] * exp( -dist2 / (2* (sig0**2 + sig[j]**2) ) )
+            # ux evaluation
+            ret[1,i] += aux1 * ret[0,i] * (xe[i]-xc[j])
+            # uy evaluation
+            ret[2,i] += aux1 * ret[0,i] * (ye[i]-yc[j])
+            # uxy evaluation
+            ret[3,i] += aux2 * ret[0,i] * (xe[i]-xc[j])*(ye[i]-yc[j])
+            # uxx evaluation (REVISAR ESTO)
+            ret[4,i] += aux2 * ret[0,i] * ((xe[i]-xc[j])**2 - sig0**2 - sig[j]**2)
+            # uyy evaluation (REVISAR ESTO)
+            ret[5,i] += aux2 * ret[0,i] * ((ye[i]-yc[j])**2 - sig0**2 - sig[j]**2)
+    return ret
+
 
 
 ###################################################################
@@ -97,7 +126,7 @@ def estimate_variance(data):
     return np.std(data)**2
 
 
-def compute_residual_stats(dfunc, c, sig, xc, yc, dims, base_level=0., square_c=True, compact_supp=True, resolution=1):
+def compute_residual_stats(dfunc, c, sig, xc, yc, dims, square_c=True, compact_supp=True, resolution=1):
     """
     Computes the residual stats between appproximation and real data
     """
@@ -110,12 +139,15 @@ def compute_residual_stats(dfunc, c, sig, xc, yc, dims, base_level=0., square_c=
     Nc = len(xc)
     Ne = len(xe)
 
+    points = np.vstack([xe,ye]).T
+    f0 = dfunc(points).reshape(len_xe,len_ye)
+
     if square_c: c = c**2
-    u = evaluate(c, sig, xe, ye, xc, yc, supp=supp, sig0=minsig) + base_level
+    u = u_eval(c, sig, xe, ye, xc, yc, supp=supp, sig0=minsig)
     u = u.reshape(len_xe, len_ye)
    
  
-    residual = dfunc(_xe, _ye)-u
+    residual = f0-u
     return (estimate_variance(residual), 
             estimate_entropy(residual),
             estimate_rms(residual))
@@ -137,3 +169,27 @@ def build_dist_matrix(points):
         Dx[k,:] = xp[k]-xp
         Dy[k,:] = yp[k]-yp
     return np.sqrt(Dx**2+Dy**2)
+
+
+def load_data(fit_path):
+    container = load_fits(fit_path)
+    data = standarize(container.primary)[0]
+    data = data.data
+
+    # stacking it
+    data = data.sum(axis=0)
+    data -= data.min()
+    data /= data.max()
+
+    # generating the data function
+    x = np.linspace(0., 1., data.shape[0]+2, endpoint=True)[1:-1]
+    y = np.linspace(0., 1., data.shape[1]+2, endpoint=True)[1:-1]
+    _dfunc = RegularGridInterpolator((x,y), data, method='linear', bounds_error=False, fill_value=0.)
+    
+    def dfunc(points):
+        if points.ndim==1:
+            return _dfunc([[points[1],points[0]]])
+        elif points.ndim==2:
+            return  _dfunc(points[:,::-1])
+    
+    return x, y, data, dfunc
