c9c2c7247d7d9d6a5f80585a9e17459a10164688
mavillan <mavillan@alumnos.inf.utfsm.cl>
2017-01-09 01:41:13 -0300

    added old versions of graph and utils to attic
    Former-commit-id: 6d5c22f24206e47277f5a700a2dbc63c451bef00

---
+++ lib/attic/graph.py
@@ -0,0 +1,263 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from utils import phi, u_eval
+
+
+##################################################################
+# GLOBAL VARIABLES
+##################################################################
+supp = 5.      # gaussians support
+minsig = 0.001 # guassians minimal broadening
+
+
+
+def plotter(dfunc, c, sig, xc, resolution=10, title=None):
+    """
+    Helper function to visualize the quality of the solution
+    """
+    _xe = np.linspace(0., 1., 10*N, endpoint=True)
+    _Dx = np.empty((10*N,N))
+    for k in range(10*N):
+        _Dx[k,:] = (_xe[k] - xc)
+
+    phi_m = phi(_Dx, sig)
+    u = np.dot(phi_m, c)
+    plt.figure(figsize=(10,6))
+    plt.plot(_xe, u, 'r-', label='Solution')
+    plt.plot(x_, dfunc(x_), 'b--', label='Data')
+    plt.plot(xe, dfunc(xe), 'go', label='Evaluation points')
+    plt.title(title)
+    plt.legend(bbox_to_anchor=(1.3, 1.0))
+    plt.show()
+
+
+
+def solution_plot(dfunc, c, sig, xc, yc, dims, base_level=0., square_c=True, mask=None, 
+                 resolution=1, title=None, compact_supp=True):
+    _xe = np.linspace(0., 1., resolution*dims[0]+2)[1:-1]
+    _ye = np.linspace(0., 1., resolution*dims[1]+2)[1:-1]
+    len_xe = len(_xe); len_ye = len(_ye)
+    Xe,Ye = np.meshgrid(_xe, _ye, sparse=False, indexing='ij')
+    xe = Xe.ravel(); ye = Ye.ravel()
+    points = np.vstack([xe,ye]).T
+    Nc = len(xc)
+    Ne = len(xe)
+
+    # approximation
+    if square_c: c = c**2
+    u = u_eval(c, sig, xe, ye, xc, yc, supp=supp, sig0=minsig) + base_level
+    u = u.reshape(len_xe, len_ye)
+
+    # real data
+    f = dfunc(points).reshape(dims)
+
+    # residual
+    res = f-u+base_level
+
+
+    # unusable pixels are fixed to 0
+    if mask is not None: 
+        u[~mask] = 0.
+        f[~mask] = 0.
+        res[~mask] = 0.
+
+
+    # original data plot
+    plt.figure(figsize=(18,12))
+    plt.subplot(1,3,1)
+    ax = plt.gca()
+    im = ax.imshow(f, vmin=0., vmax=1.)
+    plt.title('Original')
+    plt.axis('off')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+    
+    # approximated solution plot
+    plt.subplot(1,3,2)
+    ax = plt.gca()
+    im = ax.imshow(u, vmin=0., vmax=1.)
+    plt.title('Solution')
+    plt.axis('off')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+    
+    # residual plot
+    plt.subplot(1,3,3)
+    ax = plt.gca()
+    im = ax.imshow(res, vmin=0., vmax=1.)
+    plt.title('Residual')
+    plt.axis('off')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+    plt.show()
+
+
+
+def params_plot(c, sig, xc, yc, square_c=True, remove_outlier=True):
+    if square_c: c = c**2
+    if remove_outlier:
+        # just keeping values less than 10 times the mean
+        c_med = np.median(c)
+        mask1 = c < 10.*c_med
+        c_ = c[mask1]
+        xc_ = xc[mask1]; yc_ = yc[mask1]
+    plt.figure(figsize=(17,7))
+    plt.subplot(1,2,1)
+    plt.title('Plot of c parameters')
+    plt.xlim(-0.01,1.01)
+    plt.ylim(-0.01,1.01)
+    plt.scatter(yc_, xc_ , c=c_)
+    plt.gca().invert_yaxis()
+    plt.colorbar()
+    plt.subplot(1,2,2)
+    if remove_outlier:
+        # just keeping values less than 10 times the mean
+        sig2_med = np.median(sig**2)
+        mask2 = sig**2 < 10.*sig2_med
+        sig_ = sig[mask2]
+        xc_ = xc[mask2]; yc_ = yc[mask2]
+    plt.title('Plot of sig^2 parameters')
+    plt.xlim(-0.01,1.01)
+    plt.ylim(-0.01,1.01)
+    plt.scatter(yc_, xc_, c=sig_**2)
+    plt.gca().invert_yaxis()
+    plt.colorbar()
+    plt.show()
+ 
+
+def params_distribution_plot(c, sig, square_c=True, remove_outlier=True):
+    if square_c: c = c**2
+    if remove_outlier:
+        # just keeping values less than 10 times the median
+        c_med = np.median(c)
+        sig2_med = np.median(sig**2)
+        mask1 = c < 10.*c_med
+        c = c[mask1]
+        mask2 = sig**2 < 10.*sig2_med
+        sig = sig[mask2]
+    plt.figure(figsize=(14,7))
+    plt.subplot(1,2,1)
+    plt.title('C distribution')
+    plt.hist(c, color='grey', bins=10)
+    plt.subplot(1,2,2)
+    plt.title('Sig^2 distribution')
+    plt.hist(sig**2, color='grey', bins=10)
+    plt.show()
+
+
+
+def residual_plot(residual_variance, residual_entropy, residual_rms, iter_list):
+    plt.figure(figsize=(12,5))
+    plt.subplot(1,3,1)
+    plt.xlim(0, iter_list[-1]+iter_list[0])
+    plt.plot(iter_list, residual_rms, 'go-')
+    plt.title('Residual RMS')        
+    plt.subplot(1,3,2)
+    plt.xlim(0, iter_list[-1]+iter_list[0])
+    plt.plot(iter_list, residual_variance, 'bo-')
+    plt.title('Residual variance')
+    plt.subplot(1,3,3)
+    plt.xlim(0, iter_list[-1]+iter_list[0])
+    plt.plot(iter_list, residual_entropy, 'ro-')
+    plt.title('Residual entropy')
+    plt.show()
+
+
+
+def points_plot(data, center_points=None, collocation_points=None, boundary_points=None, title=None):
+    x_scale = data.shape[0]-1
+    y_scale = data.shape[1]-1
+    if (center_points is not None) and (collocation_points is None):
+        plt.figure(figsize=(8,8))
+        plt.imshow(data)
+        plt.scatter(center_points[:,1]*y_scale, center_points[:,0]*x_scale, c='r', s=5, label='center')
+        if title is not None: plt.title(title) 
+        else: plt.title('Center points')
+        plt.axis('off')
+    elif (center_points is None) and (collocation_points is not None):
+        plt.figure(figsize=(8,8))
+        plt.imshow(data)
+        plt.scatter(collocation_points[:,1]*y_scale, collocation_points[:,0]*x_scale, c='g', s=5, label='collocation')
+        if title is not None: plt.title(title)
+        else: plt.title('Collocation points')
+        plt.axis('off')
+    elif (center_points is not None) and (collocation_points is not None):
+        fig = plt.figure(figsize=(20,15))
+        ax1 = fig.add_subplot(121)
+        ax1.imshow(data)
+        ax1.scatter(center_points[:,1]*y_scale, center_points[:,0]*x_scale, c='r', s=5, label='center')
+        if title is not None: plt.title(title)
+        else: ax1.set_title('Center points')
+        ax1.axis('off')
+        ax2 = fig.add_subplot(122)
+        ax2.imshow(data)
+        ax2.scatter(collocation_points[:,1]*y_scale, collocation_points[:,0]*x_scale, c='g', s=5, label='collocation')
+        if title is not None: plt.title(title)
+        else: ax2.set_title('Collocation points')
+        ax2.axis('off')
+    if (boundary_points is not None) and len(boundary_points[:,0])!=0:
+        plt.figure(figsize=(8,8))
+        plt.imshow(data)
+        plt.scatter(boundary_points[:,1]*y_scale, boundary_points[:,0]*x_scale, c='y', s=5, label="boundary")
+        plt.axis('off')
+    #plt.colorbar(im, cax=cax)
+    #fig.legend(bbox_to_anchor=(1.2, 1.0))
+    plt.show()
+
+
+
+def components_plot(elm, data, components_dict, n_comp, dims, resolution=1, n_levels=5):
+    xc = elm.xc; yc = elm.yc; c = elm.c; sig = elm.sig
+    _xe = np.linspace(0., 1., resolution*dims[0])[1:-1]
+    _ye = np.linspace(0., 1., resolution*dims[1])[1:-1]
+    len_xe = len(_xe); len_ye = len(_ye)
+    Xe,Ye = np.meshgrid(_xe, _ye, sparse=False, indexing='ij')
+    xe = Xe.ravel(); ye = Ye.ravel()
+    Nc = len(xc)
+    Ne = len(xe)
+    
+    """ 
+    Computing distance matrices
+    """
+    #distance matrices
+    Dx = np.empty((Ne,Nc))
+    Dy = np.empty((Ne,Nc))
+    for k in range(Ne):
+        Dx[k,:] = xe[k]-xc
+        Dy[k,:] = ye[k]-yc
+    """
+    Computing the Phi matrix
+    """
+    if elm.square_c: c = c**2
+    if elm.compact_supp: phi_m = phi(Dx, Dy, sig.reshape(1,-1))
+    else: phi_m = phi(Dx, Dy, sig.reshape(1,-1), supp=0.)    
+    
+    plt.figure(figsize=(10,10))
+    plt.title('{0} components solution'.format(n_comp))
+    plt.axis('off')
+    ax = plt.subplot(1,1,1)
+    ax.imshow(data, cmap=plt.cm.gray)
+    color = plt.cm.rainbow(np.linspace(0.,1.,n_comp))
+    levels = np.linspace(1.05*elm.base_level, 0.95, n_levels)
+    
+    
+    for i,indexes in enumerate(components_dict[n_comp]):
+        _phi_m = phi_m.T[indexes].T
+        _c = c[indexes]
+        _u = np.dot(_phi_m, _c) + elm.base_level
+        _u = _u.reshape(len_xe, len_ye)
+        
+        ax.contour(_u, levels=levels, colors=[color[i]])
+        #plt.subplot(n_comp,1, i+1)
+        #ax = plt.gca()
+        #im = ax.imshow(_u, vmin=0., vmax=1.)
+        #plt.axis('off')
+        #divider = make_axes_locatable(ax)
+        #cax = divider.append_axes("right", size="5%", pad=0.05)
+        #plt.colorbar(im, cax=cax)
+    plt.show()
---
+++ lib/attic/utils.py
@@ -0,0 +1,187 @@
+import numba
+import numpy as np
+import numpy.ma as ma
+import scipy as sp
+import numexpr as ne
+from math import sqrt, exp
+
+
+@numba.jit('float64[:] (float64[:], float64[:], float64[:], float64[:], float64[:], float64[:], float64, float64)', nopython=True)
+def u_eval(c, sig, xc, yc, xe, ye, support=5):
+    m = len(xe)
+    n = len(xc)
+    ret = np.zeros(m)
+    for i in range(m):
+        for j in range(n):
+            dist2 = (xe[i]-xc[j])**2 + (ye[i]-yc[j])**2
+            if  dist2 > support**2 * sig[j]**2: continue
+            ret[i] += c[j] * exp( -dist2 / (2* sig[j]**2 ) )
+    return ret 
+
+
+@numba.jit('float64[:,:] (float64[:], float64[:], float64[:], float64[:], float64[:], float64[:], float64, float64)', nopython=True)
+def u_eval_full(c, sig, xc, yc, xe, ye, support=5):
+    m = len(xe)
+    n = len(xc)
+    ret = np.zeros((6,m))
+    for i in range(m):
+        for j in range(n):
+            dist2 = (xe[i]-xc[j])**2 + (ye[i]-yc[j])**2
+            if  dist2 > support**2 * sig[j]**2: continue
+            # common terms
+            aux1 = (-1./(sig[j]**2))
+            aux2 = aux1**2
+            # u evaluation
+            ret[0,i] += c[j] * exp( -dist2 / (2* sig[j]**2 ) )
+            # ux evaluation
+            ret[1,i] += aux1 * ret[0,i] * (xe[i]-xc[j])
+            # uy evaluation
+            ret[2,i] += aux1 * ret[0,i] * (ye[i]-yc[j])
+            # uxy evaluation
+            ret[3,i] += aux2 * ret[0,i] * (xe[i]-xc[j])*(ye[i]-yc[j])
+            # uxx evaluation (REVISAR ESTO)
+            ret[4,i] += aux2 * ret[0,i] * ((xe[i]-xc[j])**2 - sig[j]**2)
+            # uyy evaluation (REVISAR ESTO)
+            ret[5,i] += aux2 * ret[0,i] * ((ye[i]-yc[j])**2 - sig[j]**2)
+    return ret
+
+
+
+###################################################################
+# RBF (Gaussian) functions and its derivatives
+#
+# NOTE: For the compact support implementation, we
+# take the fact that gaussian functions decrease faster
+# than polynomial functions
+###################################################################
+
+
+def phi(x, y, sig, sig0=minsig, supp=5.):
+    retval = ne.evaluate('exp(-(x**2+y**2)/(2*(sig0**2+sig**2)))')
+    if supp!=0.: retval[retval < np.exp(-0.5 * supp**2)] = 0.
+    return retval
+
+def phix(x, y, sig, sig0=minsig, supp=5.):
+    retval = ne.evaluate('(-1./(sig0**2+sig**2)) * exp(-(x**2+y**2)/(2*(sig0**2+sig**2))) * x')
+    if supp!=0.: retval[retval < np.exp(-0.5 * supp**2)] = 0.
+    return retval
+
+def phiy(x, y, sig, sig0=minsig, supp=5.):
+    retval = ne.evaluate('(-1./(sig0**2+sig**2)) * exp(-(x**2+y**2)/(2*(sig0**2+sig**2))) * y')
+    if supp!=0.: retval[retval < np.exp(-0.5 * supp**2)] = 0.
+    return retval
+
+#same as phiyx
+def phixy(x, y, sig, sig0=minsig, supp=5.):
+    retval = ne.evaluate('(1./(sig0**2+sig**2)**2) * exp(-(x**2+y**2)/(2*(sig0**2+sig**2))) * (x*y)')
+    if supp!=0.: retval[retval < np.exp(-0.5 * supp**2)] = 0.
+    return retval
+
+def phixx(x, y, sig, sig0=minsig, supp=5.):
+    retval = ne.evaluate('(1./(sig0**2+sig**2)**2) * exp(-(x**2+y**2)/(2*(sig0**2+sig**2))) * (x**2 - sig0**2 - sig**2)')
+    if supp!=0.: retval[retval < np.exp(-0.5 * supp**2)] = 0.
+    return retval
+
+def phiyy(x, y, sig, sig0=minsig, supp=5.):
+    retval = ne.evaluate('(1./(sig0**2+sig**2)**2) * exp(-(x**2+y**2)/(2*(sig0**2+sig**2))) * (y**2 - sig0**2 - sig**2)')
+    if supp!=0.: retval[retval < np.exp(-0.5 * supp**2)] = 0.
+    return retval
+
+
+def estimate_rms(data):
+    """
+    Computes RMS value of an N-dimensional numpy array
+    """
+
+    if isinstance(data, ma.MaskedArray):
+        ret = np.sum(data*data) / (np.size(data) - np.sum(data.mask)) 
+    else: 
+        ret = np.sum(data*data) / np.size(data)
+    return np.sqrt(ret)
+
+
+def estimate_entropy(data):
+    """
+    Computes Entropy of an N-dimensional numpy array
+    """
+
+    # estimation of probabilities
+    p = np.histogram(data.ravel(), bins=256, density=False)[0].astype(float)
+    # little fix for freq=0 cases
+    p = (p+1.)/(p.sum()+256.)
+    # computation of entropy 
+    return -np.sum(p * np.log2(p))
+
+
+def estimate_variance(data):
+    """
+    Computes variance of an N-dimensional numpy array
+    """
+
+    return np.std(data)**2
+
+
+def compute_residual_stats(data, xc, yc, c, sig, dims, square_c=True, compact_supp=True):
+    """
+    Computes the residual stats between appproximation and real data
+    """
+
+    _xe = np.linspace(0., 1., dims[0]+2)[1:-1]
+    _ye = np.linspace(0., 1., dims[1]+2)[1:-1]
+    Xe,Ye = np.meshgrid(_xe, _ye, sparse=False, indexing='ij')
+    xe = Xe.ravel(); ye = Ye.ravel()
+
+    if square_c: c = c**2
+    u = u_eval(c, sig, xe, ye, xc, yc, supp=supp, sig0=minsig)
+    u = u.reshape(dims)
+    residual = data-u
+    
+    return (estimate_variance(residual), 
+            estimate_entropy(residual),
+            estimate_rms(residual))
+
+
+def build_dist_matrix(points):
+    """
+    Builds a distance matrix from points array.
+    It returns a (n_points, n_points) distance matrix. 
+
+    points: NumPy array with shape (n_points, 2) 
+    """
+    xp = points[:,0]
+    yp = points[:,1]
+    N = points.shape[0]
+    Dx = np.empty((N,N))
+    Dy = np.empty((N,N))
+    for k in range(N):
+        Dx[k,:] = xp[k]-xp
+        Dy[k,:] = yp[k]-yp
+    return np.sqrt(Dx**2+Dy**2)
+
+
+def load_data(fit_path):
+    container = load_fits(fit_path)
+    data = standarize(container.primary)[0]
+    data = data.data
+
+    # stacking it
+    data = data.sum(axis=0)
+    data -= data.min()
+    data /= data.max()
+
+    # generating the data function
+    x = np.linspace(0., 1., data.shape[0]+2, endpoint=True)[1:-1]
+    y = np.linspace(0., 1., data.shape[1]+2, endpoint=True)[1:-1]
+    _dfunc = RegularGridInterpolator((x,y), data, method='linear', bounds_error=False, fill_value=0.)
+    
+    def dfunc(points):
+        if points.ndim==1:
+            return _dfunc([[points[1],points[0]]])
+        elif points.ndim==2:
+            return  _dfunc(points[:,::-1])
+    
+    return x, y, data, dfunc
+
+
+def logistic(x):
+    return 1. / (1. + np.exp(-x))
