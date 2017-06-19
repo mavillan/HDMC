8bf2b185ad98c7c2e79994baa21e0f9011182132
mavillan <martin.villanueva@alumnos.usm.cl>
2017-06-19 17:22:19 -0400

    Added some fixes on variational.py module
    Former-commit-id: bdf11dd4d8703e521774922e5f5374dab3798455

--- lib/variational.py
+++ lib/variational.py
@@ -251,7 +251,7 @@ class ELModel():
 
         xc, yc, c, sig = self.get_params_mapped()
 
-        grad = grad_eval(c, sig, xc, yc, xe, ye, support=self.support) + self.base_level
+        grad = grad_eval(c, sig, xc, yc, xe, ye, support=self.support)
         grad = grad.reshape(self.dims)
 
         return grad
@@ -366,8 +366,8 @@ class ELModel():
 
 
         # evaluation points
-        #xe = np.hstack([self.xe, xc]); ye = np.hstack([self.ye, yc])
-        xe = self.xe; ye = self.ye
+        xe = np.hstack([self.xe, xc]); ye = np.hstack([self.ye, yc])
+        #xe = self.xe; ye = self.ye
         xb = self.xb; yb = self.yb
         
         # computing u, ux, uy, ...
@@ -383,8 +383,8 @@ class ELModel():
             uyy = out[5,:]
         
         # computing the EL equation
-        #f0 = np.hstack([ self.f0, self.dfunc(np.vstack([xc,yc]).T) ])
-        f0 = self.f0
+        f0 = np.hstack([ self.f0, self.dfunc(np.vstack([xc,yc]).T) ])
+        #f0 = self.f0
         a = self.a; b = self.b; lamb1 = self.lamb1; lamb2 = self.lamb2
 
         tmp1 = ne.evaluate('u-f0')
